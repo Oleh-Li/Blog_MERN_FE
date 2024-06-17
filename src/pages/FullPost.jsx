39144949877b43895289dd5ac58698a9c27a2b23
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-17 13:38:53 +0300

    fix key and MUI warnings and get data for FullPosts

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom'
 
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
+import axios from "../axios";
 
 export const FullPost = () => {
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams()
+
+  useEffect(() => {
+    axios.get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        console.warn(err)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost />
+  }
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
+        id={data._id}
+        title={data.title}
+        imageUrl={data.url}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewCount}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={data.tags}
         isFullPost
       >
         <p>
@@ -34,14 +51,14 @@ export const FullPost = () => {
         items={[
           {
             user: {
-              fullName: "Вася Пупкин",
+              fullName: "Whirun of Bligh",
               avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
             },
             text: "Это тестовый комментарий 555555",
           },
           {
             user: {
-              fullName: "Иван Иванов",
+              fullName: "Bayaz",
               avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
             },
             text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
-import axios from '../axios'; //import from folder, not library
 import { useDispatch, useSelector } from 'react-redux'
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
@@ -34,9 +33,9 @@ export const Home = () => {
           ) : (
             <Post
               key={index}
-              id={obj.id}
+              id={obj._id}
               title={obj.title}
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+              imageUrl={obj.imageUrl}
               user={obj.user}
               createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
