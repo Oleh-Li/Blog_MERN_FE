1721481999abaa4a6bfebfc971d27aa4c9f7971a
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-19 21:49:32 +0300

    added registration

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
@@ -6,22 +8,73 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
 import styles from './Login.module.scss';
+import { fetchRegister, selectIsAuth } from '../../redux/slices/auth';
+import { Navigate } from 'react-router-dom';
 
 export const Registration = () => {
+  const isAuth = useSelector(selectIsAuth)
+  const dispatch = useDispatch()
+
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: ''
+    },
+    mode: 'onChange'
+  })
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values))
+
+    if (!data.payload) {
+      return alert('Can\'t create new account')
+    }
+
+    if (data.payload.token) {
+      localStorage.setItem('token', data.payload.token)
+    }
+  }
+
+  if (isAuth) {
+    return <Navigate to={'/'} />
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
-      </Typography>
-      <div className={styles.avatar}>
-        <Avatar sx={{ width: 100, height: 100 }} />
-      </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Зарегистрироваться
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography classes={{ root: styles.title }} variant="h5">
+          Create Account
+        </Typography>
+        <div className={styles.avatar}>
+          <Avatar sx={{ width: 100, height: 100 }} />
+        </div>
+        <TextField
+          className={styles.field}
+          label="Full name"
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}
+          {...register('fullName', { required: 'Fill full name field' })}
+          fullWidth />
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Fill email field' })}
+          fullWidth />
+        <TextField
+          className={styles.field}
+          label="Password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Fill password field' })}
+          fullWidth />
+        <Button disabled={!isValid} type='submit' size="large" variant="contained" fullWidth>
+          Create Accounr
+        </Button>
+      </form>
     </Paper>
   );
 };
