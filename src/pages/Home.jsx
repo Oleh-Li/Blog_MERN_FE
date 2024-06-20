c3faf5327fe458ba55df952a984f905acd061c81
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-20 09:26:55 +0300

    fix - left etit article icon only for author of article

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import { fetchPosts, fetchTags } from '../redux/slices/posts'
 
 export const Home = () => {
   const dispatch = useDispatch()
+  const userData = useSelector(state => state.auth.data)
   const { posts, tags } = useSelector(state => state.posts)
 
   const isPostsLoading = posts.status === 'loading'
@@ -28,22 +29,23 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? (
-            <Post key={index} isLoading={true} />
-          ) : (
-            <Post
-              key={index}
-              id={obj._id}
-              title={obj.title}
-              imageUrl={obj.imageUrl}
-              user={obj.user}
-              createdAt={obj.createdAt}
-              viewsCount={obj.viewsCount}
-              commentsCount={3}
-              tags={obj.tags}
-              isEditable
-            />
-          ))}
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                key={index}
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl}
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={3}
+                tags={obj.tags}
+                isEditable={userData?._id === obj.user._id}
+              />
+            ))}
         </Grid>
         <Grid xs={4} item>
           <TagsBlock items={tags.items} isLoading={isTagsLoading} />
