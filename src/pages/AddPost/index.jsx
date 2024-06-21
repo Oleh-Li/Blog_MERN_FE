37458a0ea77f96f901e6b12d9045be6f8c54d64f
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-21 17:03:53 +0300

    added functionality to write article

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,21 +8,61 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsAuth } from '../../redux/slices/auth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import axios from '../../axios'
 
 export const AddPost = () => {
+  const navigate = useNavigate()
   const isAuth = useSelector(selectIsAuth)
-  console.log({ isAuth })
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+  const [loading, setLoading] = useState(false)
+  const [imageUrl, setImageUrl] = useState('')
+  const [text, setText] = useState('');
+  const [title, setTitle] = useState('');
+  const [tags, setTags] = useState('');
+  const inputFileRef = useRef(null)
 
-  const handleChangeFile = () => { };
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData() //web-browser object 
+      const file = event.target.files[0]
+      formData.append('image', file)
+      const { data } = await axios.post('/upload', formData)
+      setImageUrl(data.url)
+    } catch (err) {
+      console.warn(err)
+      alert('Error upload file')
+    }
+  };
 
-  const onClickRemoveImage = () => { };
+  const onClickRemoveImage = async (event) => {
+    setImageUrl('')
+  };
 
   const onChange = React.useCallback((value) => {
-    setValue(value);
+    setText(value);
   }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true)
+
+      const fields = {
+        title,
+        imageUrl,
+        tags: tags.split(','),
+        text
+      }
+
+      const { data } = await axios.post('/posts', fields)
+
+      const id = data._id
+      navigate(`/posts/${id}`)
+
+    } catch (err) {
+      console.warn(err)
+      alert('Error create post')
+    }
+  }
 
   const options = React.useMemo(
     () => ({
@@ -40,23 +80,30 @@ export const AddPost = () => {
     [],
   );
 
-  if (!isAuth) {
+  if (!localStorage.getItem('token') && !isAuth) {
     return <Navigate to='/' />
   }
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={(e) => inputFileRef.current.click()} //by button active input below with type file because it hidden 
+        variant="outlined"
+        size="large">
         Load preview
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Remove
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Remove
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
@@ -64,12 +111,20 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="The title of the article..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Tags" fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Tags"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        fullWidth />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button onClick={onSubmit} type='submit' size="large" variant="contained">
           Publish
         </Button>
         <a href="/">
